0b33f42a9c70ffd5dde3acf76cf860f2872c6ef5
JexUK <cmrxn2005@gmail.com>
2021-12-12 16:20:52 +0000

    rename folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Installation/dependencies">
+            to="/docs/install/dependencies">
             Get Started
           </Link>
         </div>
